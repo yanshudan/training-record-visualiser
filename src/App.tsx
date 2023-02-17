9c61b0f1fbe3868a5e06cfdf75a3f791bde42769
daishudan <1397370237@qq.com>
2023-02-17 18:00:45 +0800

    parse data

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { LoadTrainingRecords } from './LoadFile';
+import { RecordSerializer } from './RecordSerializer';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  return RecordTable();
 }
 
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number,
+) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = RecordSerializer.deserialize(LoadTrainingRecords());
+
+export function RecordTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Date</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.date.toString()}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.date.toDateString()}
+              </TableCell>
+              <TableCell align="right">{JSON.stringify(row.movements)}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
 export default App;
---
+++ src/RecordSerializer.ts
@@ -0,0 +1,41 @@
+export { }
+export class Record {
+    date: Date = new Date();
+    movements: (Movement | undefined)[] = [];
+}
+export class Movement {
+    name: string = "";
+    weight: number = 0;
+    unit: UnitEnum = UnitEnum.Kg;
+    reps: number[] = [];
+}
+export enum UnitEnum {
+    Kg = 1,
+    Lb = 2
+}
+export class RecordSerializer {
+    static deserialize(records: string): Record[] {
+        return records.split("\n\n").map(record => this.parseRecord(record));
+    }
+    static parseRecord(record: string): Record {
+        let lines = record.split("\n");
+        let date = new Date(lines[0].split("//")[0]);
+        let movements = lines.slice(1).map(line => this.parseMovement(line));
+        return { date, movements };
+    }
+    static parseMovement(movement: string): Movement | undefined {
+        movement = movement.split("//")[0]
+        let [meta, ...reps] = movement.split(" ");
+        let unit = meta.slice(-2) === "kg" ? UnitEnum.Kg : UnitEnum.Lb;
+        let regex = /[0-9./]/;
+
+        const res = meta.slice(0, -2)
+        const m = res.match(regex);
+        if (m === null) {
+            console.log(`Invalid movement:${movement} match:${m}`);
+            return undefined;
+        }
+        // let [name, weight, ] = meta.split(":");
+        return { name: res.slice(0, m.index), weight: Number(res.slice(m.index)), unit: Number(unit), reps: reps.map(rep => Number(rep)) };
+    }
+}
